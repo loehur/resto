10f564e687124b53210eb5b6a27b3c30f657c00c
loehur <loehur@gmail.com>
2025-06-21 12:45:39 +0700

    alpine344

--- app/Views/Approval/pengeluaran.php
+++ app/Views/Approval/pengeluaran.php
@@ -4,7 +4,7 @@
     $no = 0;
     foreach ($data['pengeluaran'] as $a) {
       $id = $a['id'];
-      $f1 = substr($a['insertTime'], 5, 11);
+      $f1 = $a['insertTime'];
       $f2 = $a['note'];
       $f2b = $a['note_primary'];
       $f4 = $a['jumlah'];
@@ -14,7 +14,7 @@
           <span class="btn btn-outline-danger" x-on:click="verify(<?= $id ?>,2)"><i class="fas fa-times"></i></span>
         </td>
         <td class="text-end">
-          <small>#<?= $id ?> <?= date('d M y, H:i', strtotime($f1)) ?> </small>
+          <small>#<?= $id ?> <?= date('d M, H:i', strtotime($f1)) ?> </small>
           <br><span class="badge bg-gradient bg-dark"><?= $f2b ?></span> <b>Rp<span><?= number_format($f4) ?></span></b></span>
           <br><small><?= $f2 ?></small>
         </td>
