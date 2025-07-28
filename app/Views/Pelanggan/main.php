5e5b420a09b6d73622a2bcdc895b2f609c113530
loehur <loehur@gmail.com>
2025-07-28 22:58:40 +0700

    benerin bayar nyasar23

--- app/Views/Pelanggan/main.php
+++ app/Views/Pelanggan/main.php
@@ -57,12 +57,6 @@
   </div>
 </div>
 
-<!-- SCRIPT -->
-<script src="<?= URL::ASSETS_URL ?>js/jquery-3.6.0.min.js"></script>
-<script src="<?= URL::ASSETS_URL ?>js/popper.min.js"></script>
-<script src="<?= URL::ASSETS_URL ?>plugins/bootstrap-5.3/js/bootstrap.bundle.min.js"></script>
-<script src="<?= URL::ASSETS_URL ?>plugins/select2/select2.min.js"></script>
-
 <script>
   $("form").on("submit", function(e) {
     e.preventDefault();
